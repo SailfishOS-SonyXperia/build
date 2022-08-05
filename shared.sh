5e601962c72ff602a1f2c70b3c2f77cdfedb722d
Björn Bidar <bjorn.bidar@thaodan.de>
2022-08-05 03:35:50 +0300

    Allow passing of OSC_PRJ in case the project isn't co yet
    Useful to parse parse a prj that hasn't been checked out yet.
    
    Signed-off-by: Björn Bidar <bjorn.bidar@thaodan.de>

--- shared.sh
+++ shared.sh
@@ -22,12 +22,12 @@ osc_parse_env()
 # Parse the %device variable from the prjconf so we now which droid-src package
 # we build for.
 {
-    local device=$(osc meta prjconf|grep 'define device'| cut -d ' ' -f3)
+    local device=$(osc meta prjconf ${OSC_PRJ} |grep 'define device'| cut -d ' ' -f3)
     vendor=$(echo $device| cut -d '-' -f1)
     family=$(echo $device| cut -d '-' -f2)
 
     # Detect the arch for the of the adaptation repo from device_rpm_architecture_string
-    adaptation_repo_arch=$(osc meta prjconf|grep device_rpm_architecture_string |
+    adaptation_repo_arch=$(osc meta prjconf ${OSC_PRJ} |grep device_rpm_architecture_string |
                            cut -d ' ' -f2)
 }
 
